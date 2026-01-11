71e090096d49c76cfb33706725f0d196d6cc408c
Vamshi0613 <ceekavamshi13@gmail.com>
2026-01-11 14:43:25 +0530

    Add cart class and upadte  methods

---
+++ cart.py
@@ -0,0 +1,32 @@
+class Cart:
+    def __init__(self, name, price,quantity, description):
+        self.name = name
+        self.price=price
+        self.quantity=quantity
+        self.description=description
+    
+    def add_item(self,product):
+        self.products.append(product)
+        print("item added successfully")
+
+    def update_item(self,name,price,quantity,description):
+        self.name=name
+        self.price=price
+        self.quantity=quantity
+        self.description=description
+        print("Items Updated Successfully")
+    
+    def info(self):
+        print("Item Name:",self.name)
+        print("Quantity:",self.quantity)
+        print("price:",self.price)
+        print("description:",self.description)
+
+mobile=Cart("apple",120500,10,"17 pro max with 128gb storage")
+
+laptop=Cart("dell",95000,5,"dell i5 generation")
+
+
+
+mobile.update_item("iphone 17promax",165000,15,"17pro max with 516gb")
+mobile.info()
